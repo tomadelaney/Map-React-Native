b0b255f61c5a7478ea701d0f408ac3221c7b15e0
Tom Delaney <tom.delaney@nyu.edu>
2018-10-23 16:55:22 -0400

    Initial Commit

---
+++ Map-React-Native/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map-React-Native
+//
+//  Created by tad on 10/23/18.
+//  Copyright © 2018 tad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
